da428f39f6d092e63f7f326a1f2553480b3f0181
basavesh <basavesh.shivakumar@gmail.com>
2021-09-08 11:07:16 +0200

    something is working wip

---
+++ compiler/practice/lucky/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// extern uint64_t encrypt_jasmin(const uint64_t msg, const uint64_t key);
+
+int main(void) {
+    printf("Hello World\n");
+    return 0;
+}
--- compiler/practice/otp/main.c
+++ compiler/practice/otp/main.c
@@ -12,13 +12,28 @@ uint64_t encrypt_c(const uint64_t msg, const uint64_t key) {
     return result;
 }
 
-extern void encrypt_jasmin(uint64_t *r0);
+uint64_t encrypt_c_2(const uint64_t msg, const uint64_t key) {
+    uint64_t result = msg;
+    for(int i=0;i < 32;i++) {
+        result ^= (key & (1 << (31 - i)));
+    }
+    return result;
+}
+
+
+
+extern uint64_t encrypt_jasmin(const uint64_t msg, const uint64_t key);
 
 int main(void) {
-    uint64_t key = 0x1d381f22be58ac3a;
-    uint64_t msg = 0x09a9d3591c6adb40;
+    const uint64_t key = 0x1d381f22be58ac3a;
+    const uint64_t msg = 0x09a9d3591c6adb40;
 
     uint64_t result_c = encrypt_c(msg, key);
+    uint64_t result_c_2 = encrypt_c_2(msg, key);
+    uint64_t result_jazz = encrypt_jasmin(msg, key);
+    printf("The C result is %lx\n", result_c);
+    printf("The C_2 result is %lx\n", result_c_2);
+    printf("The jasmin result is %lx\n\n", result_jazz);
     // declassify(msg); 
 
     for(int i=0;i<10;i++) {
